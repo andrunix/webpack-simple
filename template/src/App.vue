b4ab151cf9198006a9c03c6331d70fdb48cb0291
andrunix <andrunix@gmail.com>
2017-10-20 17:09:24 -0400

    We needed that Slash!

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <h1>\{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
